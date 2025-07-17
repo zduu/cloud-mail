4a90b6709d0cdaa5b9e870162cacc232d0e4a133
eoao <chifan6432@gmail.com>
2025-07-18 01:16:44 +0800

    修改版本号

--- mail-vue/src/views/sys-setting/index.vue
+++ mail-vue/src/views/sys-setting/index.vue
@@ -267,7 +267,7 @@
             <div class="card-content">
               <div class="concerning-item">
                 <span>版本:</span>
-                <span>v1.3.1</span>
+                <span>v1.4.0</span>
               </div>
               <div class="concerning-item">
                 <span>交流:</span>
